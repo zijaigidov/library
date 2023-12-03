212ed1db04fe3979fa41774d8011aedf8115b6d1
zijaigidov <igidov.zija@gmail.com>
2023-12-03 13:36:50 +0100

    Add client-side validation to book form
    - Added error messages to be displayed whenever the user submits
      the form with invalid input
    - Added validation styles to the input fields
    - Added JavaScript validation with the Constraint Validation API

--- app.js
+++ app.js
@@ -2,12 +2,78 @@
 const bookModal = document.querySelector('dialog');
 const btnOpenModal = document.getElementById('btn-open-modal');
 const btnCloseModal = document.getElementById('btn-close-modal');
-const bookForm = document.getElementById('book-form');
 
 btnOpenModal.addEventListener('click', () => bookModal.showModal());
 btnCloseModal.addEventListener('click', () => bookModal.close());
 
-bookForm.addEventListener('submit', (e) => {
+// Validation for book form
+const bookForm = document.getElementById('book-form');
+const bookTitle = document.getElementById('book-title');
+const bookAuthor = document.getElementById('book-author');
+const bookPages = document.getElementById('book-pages');
+const bookImageURL = document.getElementById('book-image-url');
+
+const validatableInputs = [bookTitle, bookAuthor, bookPages, bookImageURL];
+const errorMessages = bookForm.querySelectorAll('.error-msg');
+const errorMessagesMap = new Map(
+  validatableInputs.map((input, idx) => [input, errorMessages[idx]]),
+);
+
+bookForm.addEventListener('submit', (e) => handleFormSubmission(e));
+
+function handleFormSubmission(e) {
+  // Prevent the modal from automatically closing on submit
   e.preventDefault();
-  bookModal.close();
+  clearValidation();
+  setDefaultValidation();
+
+  validateInput(bookTitle);
+  validateInput(bookAuthor);
+  validateInput(bookPages);
+  validateInput(bookImageURL);
+
+  if (!bookForm.checkValidity()) {
+  } else {
+    bookModal.close();
+    bookForm.reset();
+  }
+}
+
+// Clear the validation styles and input fields when the user closes the modal
+bookModal.addEventListener('close', () => {
+  clearValidation();
+  bookForm.reset();
 });
+
+function validateInput(input) {
+  if (!input.checkValidity()) {
+    input.classList.replace('valid', 'invalid');
+    const errorElement = errorMessagesMap.get(input);
+    errorElement.style.visibility = 'visible';
+    errorElement.textContent = getErrorMessage(input);
+  }
+}
+
+function getErrorMessage(input) {
+  let errorMessage = '';
+
+  if (input.validity.valueMissing) {
+    errorMessage = 'Please fill in the input field';
+  } else if (input.validity.patternMismatch || input.validity.typeMismatch) {
+    errorMessage = `Please fill in a valid ${input.getAttribute(
+      'data-input-type',
+    )}`;
+  }
+  return errorMessage;
+}
+
+function clearValidation() {
+  errorMessagesMap.forEach((msg, input) => {
+    input.classList.remove('valid', 'invalid');
+    msg.style.visibility = 'hidden';
+  });
+}
+
+function setDefaultValidation() {
+  validatableInputs.forEach((input) => input.classList.add('valid'));
+}
