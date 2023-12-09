f82fea79366b76ef4f7b0f66aa4049c5ab9d7bd4
zijaigidov <igidov.zija@gmail.com>
2023-12-09 17:00:22 +0100

    Add logic for creating book cards
    Added the JavaScript logic necessary for the user's input to be
    converted to an element in the document.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 // ***** LIBRARY AND BOOK LOGIC *****
 
 const library = [];
+const main = document.querySelector('.main');
 
 function Book(title, author, pages, imageURL, isRead) {
   this.title = title;
@@ -8,10 +9,67 @@ function Book(title, author, pages, imageURL, isRead) {
   this.pages = pages;
   this.imageURL = imageURL;
   this.isRead = isRead;
+
+  this.cardElement = this.createBookCard();
+  this.addToLibrary();
+  this.displayBook();
 }
 
+// Function for adding a book to the library
 Book.prototype.addToLibrary = function () {
   library.push(this);
+};
+
+// Function for creating a book card
+Book.prototype.createBookCard = function () {
+  const card = document.createElement('article');
+  card.classList.add('book-card');
+
+  const cover = document.createElement('div');
+  cover.classList.add('book-cover');
+  cover.style.backgroundImage = `url('${this.imageURL}')`;
+  card.appendChild(cover);
+
+  const info = document.createElement('div');
+  info.classList.add('book-info');
+  card.appendChild(info);
+
+  const title = document.createElement('h3');
+  title.textContent = this.title;
+  title.classList.add('title');
+  info.appendChild(title);
+
+  const author = document.createElement('p');
+  author.textContent = this.author;
+  author.classList.add('author');
+  info.appendChild(author);
+
+  const pages = document.createElement('p');
+  pages.textContent = `${this.pages} pages`;
+  info.appendChild(pages);
+
+  const removeBtn = document.createElement('button');
+  removeBtn.classList.add('btn-remove');
+  const removeIcon = document.createElement('i');
+  removeIcon.classList.add('fa-solid', 'fa-trash');
+  removeBtn.appendChild(removeIcon);
+  info.appendChild(removeBtn);
+
+  const statusBtn = document.createElement('button');
+  statusBtn.classList.add('btn-status');
+  const statusIcon = document.createElement('i');
+
+  if (this.isRead) statusIcon.classList.add('fa-solid', 'fa-circle-check');
+  else statusIcon.classList.add('fa-solid', 'fa-circle-xmark');
+
+  statusBtn.appendChild(statusIcon);
+  info.appendChild(statusBtn);
+
+  return card;
+};
+
+Book.prototype.displayBook = function () {
+  main.appendChild(this.cardElement);
 };
 
 // ***** MODAL FOR ADDING BOOK *****
@@ -60,7 +118,6 @@ function handleFormSubmission(e) {
       bookImageURL.value,
       bookIsRead.checked,
     );
-    newBook.addToLibrary();
 
     bookModal.close();
     bookForm.reset();
